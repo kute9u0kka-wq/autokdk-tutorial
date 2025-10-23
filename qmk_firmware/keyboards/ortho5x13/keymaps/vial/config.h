c564fb0d13068b6329c38bf3dabd7a2886e1de05
kute9u0kka-wq <kute9u0kka@gmail.com>
2025-10-24 00:31:58 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/ortho5x13/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x85, 0x35, 0x60, 0xb9, 0xfd, 0x13, 0x45, 0xc3}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
